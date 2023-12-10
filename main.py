3ba79e46c42a19e366db0e4dfcf34ad3667a5dc2
Editxd <152770568+Editxd@users.noreply.github.com>
2023-12-10 12:21:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ bot = Client("bot",
              bot_token=os.environ.get("BOT_TOKEN"),
              api_id=int(os.environ.get("API_ID")),
              api_hash=os.environ.get("API_HASH"))
-auth_users = [6172397014,5699060814]
+auth_users = [1476002847,5699060814]
 sudo_users = auth_users
 sudo_groups = [-4005748655]
 
